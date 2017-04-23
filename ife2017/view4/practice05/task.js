fd908f10d729bbc1324c13ee5a1083a12f07dce8
Your Name <cwj0229@outlook.com>
2017-04-23 10:28:28 +0800

    view p5

--- ife2017/view4/practice05/task.js
+++ ife2017/view4/practice05/task.js
@@ -7,7 +7,6 @@ var deg = 0; //初始化角度
 var face = 0; //初始化方向  0: 上, 1: 右, 2: 下, 3: 左;
 var xPos =  300;//初始化X距离
 var yPos =  300;//初始化Y距离
-
 //创建表格 以及初始化方块位置
 (function createTableBox() {
     var table = document.getElementById('table');
@@ -33,7 +32,6 @@ var yPos =  300;//初始化Y距离
     robot.style.left= xPos+ "px";
     robot.style.top= yPos+ "px";
 })();
-
 var root = {
     //X距离
     xPos: function() {
@@ -103,6 +101,31 @@ var root = {
             case "TUN BAC":
                 root.turnBack();
                 break;
+            //新加指令
+            case "TRA LEF":
+                root.ToLeft();
+                break;
+            case "TRA TOP":
+                root.ToTop();
+                break;
+            case "TRA RIG":
+                root.ToRight();
+                break;
+            case "TRA BOT":
+                root.ToBottom();
+                break;
+            case "MOV LEF":
+                root.MoveLeft();
+                break;
+            case "MOV TOP":
+                root.MoveTop();
+                break;
+            case "MOV RIG":
+                root.MoveRight();
+                break;
+            case "MOV BOT":
+                root.MoveBottom();
+                break;
         }
     },
     turnLeft: function() {
@@ -125,5 +148,82 @@ var root = {
         face += 2;
         root.face()
     },
+    ToLeft: function(){
+        if (xPos > 50) {
+            xPos -= 50;
+            robot.style.left = xPos + 'px';
+        }
+    },
+    ToTop: function(){
+        if (yPos > 50) {
+            yPos -= 50;
+            robot.style.top = yPos + 'px';
+        }
+    },
+    ToRight:function(){
+        if (xPos < 500) {
+            xPos += 50;
+            robot.style.left = xPos + 'px';
+        }
+    },
+    ToBottom:function(){
+        if (yPos < 500) {
+            yPos += 50;
+            robot.style.top = yPos + 'px';
+        }
+    },
+    MoveLeft:function(){
+        //如果方向不同
+        if (face != 1) {
+            var little = setInterval(function() {
+                root.turnLeft();
+                if (face == 1) {
+                    clearInterval(little);
+                }
+            }, 50);
+            setTimeout("root.ToLeft()",1000);
+        } else {
+            setTimeout("root.ToLeft()",1000);
+        }
+    },
+    MoveTop:function(){
+        if (face != 0) {
+            var little = setInterval(function() {
+                root.turnLeft();
+                if (face == 0) {
+                    clearInterval(little);
+                }
+            }, 50);
+            setTimeout("root.ToTop()",1000);
+        } else {
+            setTimeout("root.ToTop()",1000);
+        }
+    },
+    MoveRight:function(){
+        if (face != 3) {
+            var little = setInterval(function() {
+                root.turnRight();
+                if (face == 3) {
+                    clearInterval(little);
+                }
+            }, 50);
+            setTimeout("root.ToRight()",1000);
+        } else {
+            setTimeout("root.ToRight()",1000);
+        }
+    },
+    MoveBottom:function(){
+        if (face != 2) {
+            var little = setInterval(function() {
+                root.turnRight();
+                if (face == 2) {
+                    clearInterval(little);
+                }
+            }, 50);
+            setTimeout("root.ToBottom()",1000);
+        } else {
+            setTimeout("root.ToBottom()",1000);
+        }
+    }
 }
 cmd_btn.addEventListener("click", root.go, false)
